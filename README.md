07ddf8ca9ddb45d79b82875dc12ac0b87dabeceb
kivaimuinde <muindejosphat@gmail.com>
2024-05-13 06:55:14 +0300

    Task E commited

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # My Car Shop
 
-## Task C
+## Task C amaking changes to the website pages
 In this task, I customized the website pages. The changes can be reached in the mainscreen.html on lines 14,19,21, 35,37, 53, and 71-73
 
 
-## task D
+## task D Adding About us Page
 Added the about us page ( seee aboutus.html). I also added the url mapping in the MainScreenController.java in lines 56-59
 A link to the aboutus page was added to mainscreen.html in lines 90-92. A commit was carried out for tasks C and D with the message 'Tasks C and D'
+
+
+## Task E Adding inventory
+Mock inventory was added on the BootStrapData.java in lines 70-132. The inventory comprises 5 products and 5 parts.
+The inventory is only created when the database is empty. A git commit was carried for task E with the message 'Task E Completed'
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -67,6 +67,69 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(unicycle);
         */
 
+        if (productRepository.count()==0){
+            //There are no products
+            Product BMW= new Product("BMW",100200.0,15);
+            Product Toyota= new Product("Toyota",10500.0,20);
+            Product Honda= new Product("Honda",50000.0,50);
+            Product Acura= new Product("Acura",65000.0,100);
+            Product Ford= new Product("Ford",80000.0,30);
+
+            productRepository.save(BMW);
+            productRepository.save(Toyota);
+            productRepository.save(Honda);
+            productRepository.save(Acura);
+            productRepository.save(Ford);
+        }
+
+
+        if (outsourcedPartRepository.count()==0){
+            //There are no out sourced parts
+
+            OutsourcedPart out_radiator= new OutsourcedPart();
+            out_radiator.setCompanyName("Company 1");
+            out_radiator.setName("radiator");
+            out_radiator.setInv(5);
+            out_radiator.setPrice(20.0);
+            out_radiator.setId(100L);
+
+            OutsourcedPart out_battery= new OutsourcedPart();
+            out_battery.setCompanyName("Company 2");
+            out_battery.setName("battery");
+            out_battery.setInv(5);
+            out_battery.setPrice(20.0);
+            out_battery.setId(100L);
+
+            OutsourcedPart out_door= new OutsourcedPart();
+            out_door.setCompanyName("Company 3");
+            out_door.setName("door");
+            out_door.setInv(5);
+            out_door.setPrice(20.0);
+            out_door.setId(100L);
+
+            OutsourcedPart out_engine= new OutsourcedPart();
+            out_engine.setCompanyName("COmpany 4");
+            out_engine.setName("engine");
+            out_engine.setInv(5);
+            out_engine.setPrice(20.0);
+            out_engine.setId(100L);
+
+            OutsourcedPart out_transmission= new OutsourcedPart();
+            out_transmission.setCompanyName("COmpany 5");
+            out_transmission.setName("transmission");
+            out_transmission.setInv(5);
+            out_transmission.setPrice(20.0);
+            out_transmission.setId(100L);
+
+
+            outsourcedPartRepository.save(out_radiator);
+            outsourcedPartRepository.save(out_door);
+            outsourcedPartRepository.save(out_engine);
+            outsourcedPartRepository.save(out_transmission);
+            outsourcedPartRepository.save(out_battery);
+
+        }
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
         System.out.println(productRepository.findAll());
